3207f6673f1071822b96ecad8b23fe60774661fd
lomic <lomic@camillesps-MacBook-Pro.local>
2025-07-23 13:36:38 +0200

    Add abundance matrix with 4 isotopes, use for single spike incubation

--- R/abundance_treatment.R
+++ R/abundance_treatment.R
@@ -6,16 +6,18 @@ abun_treat=function(z) {
     abun.2 <- as.data.frame(t(abun.1))%>%
       tibble::rownames_to_column()%>%
       tidyr::separate(rowname, c('isotope', 'number'), sep = '_')
-    abun.3 <- abun.2%>%
-      dplyr::select(1:2 |contains(paste(abun.2[1,2]))|contains(paste(abun.2[2,2]))|
-               contains(paste(abun.2[3,2]))|contains(paste(abun.2[4,2]))|
-               contains(paste(abun.2[5,2])))%>%
-      tidyr::unite('isotope', c(isotope, number), sep = '_')
+    numrows <- nrow(abun.2)
+    if (numrows == 5 ) {
+      abun.3 <- abun.2%>%
+        dplyr::select(1:2 |contains(paste(abun.2[1,2]))|contains(paste(abun.2[2,2]))|
+                        contains(paste(abun.2[3,2]))|contains(paste(abun.2[4,2]))|
+                        contains(paste(abun.2[5,2])))%>%
+        tidyr::unite('isotope', c(isotope, number), sep = '_')
 
-    abun.4 <- as.data.frame(t(abun.3))
-    colnames(abun.4) <- abun.4[1,]
-    abun.4 <- abun.4[-1,]
-    abun.5 <- as.data.frame(lapply(abun.4, function(x) as.numeric(as.character(x))))
+      abun.4 <- as.data.frame(t(abun.3))
+      colnames(abun.4) <- abun.4[1,]
+      abun.4 <- abun.4[-1,]
+      abun.5 <- as.data.frame(lapply(abun.4, function(x) as.numeric(as.character(x))))
 
       # Construct a 5x5 matrix for ratios_matrix when numcols1 != numcols2
       ratios_matrix <- c(
@@ -26,6 +28,28 @@ abun_treat=function(z) {
         abun.5[,5]/abun.5[5,5]
       )
       ratios_matrix <- matrix(ratios_matrix, nrow = 5, ncol = 5, byrow = TRUE)
+
+    } else {
+      abun.3 <- abun.2%>%
+        dplyr::select(1:2 |contains(paste(abun.2[1,2]))|contains(paste(abun.2[2,2]))|
+                        contains(paste(abun.2[3,2]))|contains(paste(abun.2[4,2])))%>%
+        tidyr::unite('isotope', c(isotope, number), sep = '_')
+
+      abun.4 <- as.data.frame(t(abun.3))
+      colnames(abun.4) <- abun.4[1,]
+      abun.4 <- abun.4[-1,]
+      abun.5 <- as.data.frame(lapply(abun.4, function(x) as.numeric(as.character(x))))
+
+      # Construct a 4x4 matrix for ratios_matrix when numcols1 != numcols2
+      ratios_matrix <- c(
+        abun.5[,1]/abun.5[1,1],
+        abun.5[,2]/abun.5[2,2],
+        abun.5[,3]/abun.5[3,3],
+        abun.5[,4]/abun.5[4,4]
+      )
+      ratios_matrix <- matrix(ratios_matrix, nrow = 4, ncol = 4, byrow = TRUE)
+
+    }
 
     # Check if ratios_matrix is square and contains no NaN or Inf values before solving
     if (nrow(ratios_matrix) != ncol(ratios_matrix)) {
